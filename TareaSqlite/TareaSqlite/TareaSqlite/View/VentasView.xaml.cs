05c15da0704e6701e9037ea8b864f9e8a813a252
josernesto <jecm3>
2019-06-27 15:38:28 -0400

    commit con carpetas creadas

---
+++ TareaSqlite/TareaSqlite/TareaSqlite/View/VentasView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TareaSqlite.View
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+   
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VentasView : ContentPage
+	{
+		public VentasView ()
+		{
+			InitializeComponent ();
+           
+        }
+	}
+}
